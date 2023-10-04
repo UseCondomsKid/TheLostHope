70c9089f1afc943a641a142e2d1102ff1e5cc38a
Amen <53493383+UseCondomsKid@users.noreply.github.com>
2023-10-05 00:08:18 +0100

    changes

--- Content/LDtkTypes/World/Enums/GunBulletType.cs
+++
@@ -1,9 +0,0 @@
-// This file was automatically generated, any modifications will be lost!
-#pragma warning disable
-namespace LDtkTypes;
-
-public enum GunBulletType
-{
-    Hitscan,
-}
-#pragma warning restore
--- Program.cs
+++
@@ -1,2 +0,0 @@
-﻿using var game = new TheLostHope.Game1();
-game.Run();
